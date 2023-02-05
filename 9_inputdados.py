9fc831acbe636e4f9324713f0852828896dd1113
Gabriqm <124095027+Gabriqm@users.noreply.github.com>
2023-02-05 01:52:02 -0300

    Update 9_inputdados.py

--- 9_inputdados.py
+++ 9_inputdados.py
@@ -1,6 +1,6 @@
-#idade = input("Qual a sua idade?")
+idade = input("Qual a sua idade?")
 
-#print ("Ah, então você tem %s anos." %idade)
+print ("Ah, então você tem %s anos." %idade)
 
 nome = input("Insira seu nome: ")
 print("Olá %s, seja muito bem vindo!" %nome)
