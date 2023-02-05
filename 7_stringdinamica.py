15cd8ce6d85ec3930ca7e2c3ff4613bd74e48b7e
Gabriqm <124095027+Gabriqm@users.noreply.github.com>
2023-02-05 01:51:32 -0300

    Update 7_stringdinamica.py

--- 7_stringdinamica.py
+++ 7_stringdinamica.py
@@ -1,33 +1,33 @@
-#name = "Gabriel"
+name = "Gabriel"
 
-#print("Olá, meu nome é %s" % name)
+print("Olá, meu nome é %s" % name)
 
-#idade = 25
+idade = 25
 
-#print("Eu tenho %d anos" % idade)
+print("Eu tenho %d anos" % idade)
 
-#carro = "ferrari"
-#ano = 2000
+carro = "ferrari"
+ano = 2000
 
-#print("O meu carro é uma %s dos anos %d" % (carro, ano))
+print("O meu carro é uma %s dos anos %d" % (carro, ano))
 
 #agora com floats, temos as casas decimais, %.2f (duas casas decimais), seu padrão em %f são 6 casas decimais.
 
-#pi = 3.141517487480
+pi = 3.141517487480
 
-#print("O valor de pi é %.2f" %pi)
+print("O valor de pi é %.2f" %pi)
 
-#motor = 2.0
+motor = 2.0
 
-#print("O motor do meu carro é %f" %motor)
+print("O motor do meu carro é %f" %motor)
 
 #Exercício 1:
 
-#nome = "Gabriel"
-#idade = 25 
-#dinheiro = 725.16
+nome = "Gabriel"
+idade = 25 
+dinheiro = 725.16
 
-#print("olá, %s, você tem %d anos e possui no banco %.3f reais" %(nome, idade, dinheiro))
+print("olá, %s, você tem %d anos e possui no banco %.3f reais" %(nome, idade, dinheiro))
 
 #Exercício 2:
 
