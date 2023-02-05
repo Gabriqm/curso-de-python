f1547e9e4fa2b1d0f7acd60f4d4190e46afad7b6
Gabriqm <124095027+Gabriqm@users.noreply.github.com>
2023-02-05 01:39:43 -0300

    Update 15_while.py

--- 15_while.py
+++ 15_while.py
@@ -1,47 +1,47 @@
-#print("Antes do loop")
+print("Antes do loop")
 
-#numero = 0
+numero = 0
 
-#while numero < 5:
-#    print(numero)
-#    if numero == 3:
-#        print("O número é 3!")
-#    numero = numero + 1
+while numero < 5:
+    print(numero)
+    if numero == 3:
+        print("O número é 3!")
+    numero = numero + 1
 
-#print ("Depois do loop")
+print ("Depois do loop")
 
 #Exercício 1:
 
-#numero = int(input("digite o número 10: "))
+numero = int(input("digite o número 10: "))
 
 
-#while numero <=50:
-#    print(numero)
-#    numero = numero + 1
+while numero <=50:
+    print(numero)
+    numero = numero + 1
 
-#print("Acabou o loop!")
+print("Acabou o loop!")
 
 
 #Exercício 2:
 
-#x = int(input("Digite aqui o número 20: "))
+x = int(input("Digite aqui o número 20: "))
 
-#while x >= 0:
-#    print(x)
-#    x = x - 1
+while x >= 0:
+    print(x)
+    x = x - 1
     
 
-#print("Acabou o loop")
+print("Acabou o loop")
 
 
 #Exercício 3:
 
-#x = int(input("Insira um número: "))
+x = int(input("Insira um número: "))
 
-#y = 0 
-#while y <= x:
-#    print(y)
-#    y = y + 1
+y = 0 
+while y <= x:
+    print(y)
+    y = y + 1
 
 
 #Exercício 4 - Pares de 1 a 50:
