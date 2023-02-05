81b653cbd6e4d4b0b9256c9e95dc0329baf458da
Gabriqm <124095027+Gabriqm@users.noreply.github.com>
2023-02-05 01:38:22 -0300

    Update 14_elif.py

--- 14_elif.py
+++ 14_elif.py
@@ -1,50 +1,50 @@
-#nome = input("Digite o seu nome: ")
+nome = input("Digite o seu nome: ")
 
-#if nome == "Gabriel":
-#    print("Olá, você é um admin!")
+if nome == "Gabriel":
+    print("Olá, você é um admin!")
     
-#    if 5>10:
-#        print("Testando")
-#    elif 10 > 5:
-#        print("Você caiu aqui") 
+    if 5>10:
+        print("Testando")
+    elif 10 > 5:
+        print("Você caiu aqui") 
 
-#elif nome == "João":
-#    print("Olá, você é um produtor de conteúdo!")
-#else:
-#    print("Você é um usuário comum!")
+elif nome == "João":
+    print("Olá, você é um produtor de conteúdo!")
+else:
+    print("Você é um usuário comum!")
 
 
-#numero = int(input("Digite um número: "))
-#print(numero)
+numero = int(input("Digite um número: "))
+print(numero)
 
-#if numero > 0 and numero <= 5:
-#    print("Maior que 0")
-#elif numero > 5 and numero <= 10:
-#    print("Maior que 5")
-#elif numero > 10 and numero <= 20:
-#    print("Maior que 10")
-#elif numero > 20:
-#    print("Maior que 20")
+if numero > 0 and numero <= 5:
+    print("Maior que 0")
+elif numero > 5 and numero <= 10:
+    print("Maior que 5")
+elif numero > 10 and numero <= 20:
+    print("Maior que 10")
+elif numero > 20:
+    print("Maior que 20")
 
-#else:
-#    print("Número negativo")
+else:
+    print("Número negativo")
 
 
 #Exercício 1:
 
-#categoria = int(input("Qual a categoria do seu produto?"))
+categoria = int(input("Qual a categoria do seu produto?"))
 
-#print(categoria)
+print(categoria)
 
-#if categoria == 1:
-#    print("Seu produto é uma bolsa.")
-#elif categoria == 2:
-#    print("Seu produto é um tênis.")
-#elif categoria == 3:
-#    print("seu produto é uma mochila.")
+if categoria == 1:
+    print("Seu produto é uma bolsa.")
+elif categoria == 2:
+    print("Seu produto é um tênis.")
+elif categoria == 3:
+    print("seu produto é uma mochila.")
 
-#else:
-#    print("A categoria não foi encontrada.")
+else:
+    print("A categoria não foi encontrada.")
 
 #Exercício 2:
 
