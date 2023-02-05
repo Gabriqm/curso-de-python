8abe2861e433d390fdcf83d07d441e7ce1f76e72
Gabriqm <124095027+Gabriqm@users.noreply.github.com>
2023-02-05 01:48:47 -0300

    Update 4_operadoreslogicos.py

--- 4_operadoreslogicos.py
+++ 4_operadoreslogicos.py
@@ -1,40 +1,40 @@
 #operador not (inverte um valor booleano)
 
-#verdadeiro = True
-#falso = False
+verdadeiro = True
+falso = False
 
-#print(not verdadeiro)
-#print(not falso)
+print(not verdadeiro)
+print(not falso)
 
-#print(not(5>2))
-#print(not(5<2))
+print(not(5>2))
+print(not(5<2))
 
 #operador and (compara um booleano com outro, só retorna true se ambos forem true)
 
-#a = 5
-#b = 10
-#c = 2
-#d = 8
+a = 5
+b = 10
+c = 2
+d = 8
 
-#print(a>b and c>d)
-#print(a>b and c<d)
-#print(c<d and b<c)
-#print(a<b and b>c)
+print(a>b and c>d)
+print(a>b and c<d)
+print(c<d and b<c)
+print(a<b and b>c)
 
 #operador or (compara um booleano com outro, só retorna false se ambos forem false)
 
-#a = 5
-#b = 10
+a = 5
+b = 10
 
-#print(a > b or b == 11)
-#print(b>a or b ==10)
-#print(a>b or b==10)
-#print(b>a or b==20)
+print(a > b or b == 11)
+print(b>a or b ==10)
+print(a>b or b==10)
+print(b>a or b==20)
 
-#nome = "Gabriel"
-#idade = 25
+nome = "Gabriel"
+idade = 25
 
-#print(nome == "Pedro" or idade >20)
+print(nome == "Pedro" or idade >20)
 
 x = True
 y = False
