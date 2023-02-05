ba33f543e4f7f3a7692b421ec962163679d92e81
Gabriqm <124095027+Gabriqm@users.noreply.github.com>
2023-02-05 01:40:42 -0300

    Update 16_break.py

--- 16_break.py
+++ 16_break.py
@@ -1,42 +1,42 @@
-#numero = 0
+numero = 0
 
-#while numero < 10:
-#    print(numero)
+while numero < 10:
+    print(numero)
 
-#    if numero == 5:
-#        break
+    if numero == 5:
+        break
 
 
-#    numero = numero + 1
+    numero = numero + 1
     
-#print("Continuou após o loop")
+print("Continuou após o loop")
 
 #Exercício 1:
 
-#numero = 20
+numero = 20
 
-#while numero >= 0:
-#    print(numero)
+while numero >= 0:
+    print(numero)
 
-#    if numero == 5:
-#        break
+    if numero == 5:
+        break
 
-#    numero = numero - 1
+    numero = numero - 1
 
-#print("Pós loop")
+print("Pós loop")
 
 #Exercício 2 - Digitar numeros em loop e se for 0 encerrar o programa:
 
-#n = int(input("Digite um número: "))
+n = int(input("Digite um número: "))
 
-#while n > 0:
-#    print(n)
-#    n = int(input("Digite mais um número: "))
+while n > 0:
+    print(n)
+    n = int(input("Digite mais um número: "))
 
-#    if n == 0:
+    if n == 0:
 
-#        break
+        break
 
-#print("Acabou o loop")
+print("Acabou o loop")
 
 
