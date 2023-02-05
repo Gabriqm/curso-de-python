dc41966c0aa9bf9ba4c53a07355900952c16c9ec
Gabriqm <124095027+Gabriqm@users.noreply.github.com>
2023-02-05 01:43:40 -0300

    Update 20_percorrerlistas.py

--- 20_percorrerlistas.py
+++ 20_percorrerlistas.py
@@ -1,38 +1,38 @@
-#lista = ["Maçã", "Uva", "Mamão", "Goiaba"]
+lista = ["Maçã", "Uva", "Mamão", "Goiaba"]
 
-#i = 0
+i = 0
 
-#while i<4:
-#    print(lista[i])
-#    i = i + 1
+while i<4:
+    print(lista[i])
+    i = i + 1
 
 #Exercício 1 loop que calcula média de 5 notas:
 
-#notas = [3,9,5,6,2]
+notas = [3,9,5,6,2]
 
-#i = 0
+i = 0
 
-#soma = 0
+soma = 0
 
-#while i<5:
-#    soma = soma + notas[i]
-#    i = i + 1
+while i<5:
+    soma = soma + notas[i]
+    i = i + 1
 
-#media = soma/5
+media = soma/5
 
-#print("O alune teve a média %.1f em física" %media)
+print("O alune teve a média %.1f em física" %media)
 
 
 #Exercício 2 loop pra preencher lista com valores zerados inseridos pelo usuario:
 
-#lista = [0,0,0,0,0]
+lista = [0,0,0,0,0]
 
-#print(lista)
+print(lista)
 
-#i = 0
+i = 0
 
-#while i < 5:
-#    lista[i] = int(input("Insira o número %d da lista: "%i))
+while i < 5:
+    lista[i] = int(input("Insira o número %d da lista: "%i))
     
 #    i = i + 1
 
